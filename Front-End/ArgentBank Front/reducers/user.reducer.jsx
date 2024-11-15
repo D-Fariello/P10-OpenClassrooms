19c8eca83e722d9cbf691c42914ff93d465e5561
Dalila <dalila.fariello@gmail.com>
2024-11-15 16:58:22 +0100

    feat: persist user authentication with localStorage

--- Front-End/ArgentBank Front/reducers/user.reducer.jsx
+++ Front-End/ArgentBank Front/reducers/user.reducer.jsx
@@ -8,11 +8,13 @@ const initialState = {
 export default function userReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_TOKEN:
+      localStorage.setItem("token", action.payload);
       return {
         ...state,
         token: action.payload,
       };
     case FETCH_USER_DATA:
+      localStorage.setItem("user", JSON.stringify(action.payload));
       return {
         ...state,
         user: action.payload,
