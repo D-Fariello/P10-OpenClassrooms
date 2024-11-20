2feaa95123401b07c24f2edec9cee0e39841783e
Dalila <dalila.fariello@gmail.com>
2024-11-20 15:02:42 +0100

    chore: Remove unnecessary console.log statements

--- Front-End/ArgentBank Front/src/components/UserGreeting.jsx
+++ Front-End/ArgentBank Front/src/components/UserGreeting.jsx
@@ -4,7 +4,6 @@ import { editUserName } from "../../actions/user.actions";
 
 const UserGreeting = () => {
   const user = useSelector((state) => state.userReducer);
-  console.log(user);
   const [editButton, setEditButton] = useState(false);
   const [userName, setUserName] = useState(user?.user?.body?.userName || "");
   const dispatch = useDispatch();
